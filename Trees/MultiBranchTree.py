03f77a594072d7030be1d4502afe221444a31e48
RYSL <32707042+SlaterGitHub@users.noreply.github.com>
2019-09-19 19:28:44 +0100

    added breadth and depth print

--- Trees/MultiBranchTree.py
+++ Trees/MultiBranchTree.py
@@ -1,15 +1,18 @@
 class Node:
     def __init__(self, node):
         self.branches = []
-        self.node = Node
+        self.content = node
 
 class tree:
     def __init__(self, node):
-        self.tree = Node(node)
+        self.node = Node(node)
 
     def addNode(self, path, node, currentNode):
-        if path == currentNode:
-            currentNode.branches.append(node)
+        if currentNode == None:
+            currentNode = self.node
+        words = path.split()
+        if words[len(words)-1] == currentNode.content:
+            currentNode.branches.append(Node(node))
             return
         branchToGo = self.pathLocation(path, currentNode.branches)
         if branchToGo == -1:
@@ -19,9 +22,37 @@ class tree:
     def pathLocation(self, path, branches):
         x = 0
         while(x < len(branches)):
-            if braches[x] in path:
+            if branches[x].content in path:
                 break
             x+=1
-        if x = len(branches):
+        if x == len(branches):
             return -1
         return x
+
+    def printTree(self, searchType):
+        if searchType == "breadth":
+            return self.breadthPrint(self.node, [])
+        else:
+            return self.depthPrint(self.node, [])
+
+    def depthPrint(self, node, nodes):
+        for x in range(len(node.branches)):
+            nodes = self.depthPrint(node.branches[x], nodes)
+        nodes.append(node.content)
+        return nodes
+
+    def breadthPrint(self, node, nodes):
+        nodes.append(node.content)
+        for x in range(len(node.branches)):
+            nodes = self.breadthPrint(node.branches[x], nodes)
+        return nodes
+
+
+
+folders = tree("Documents")
+folders.addNode("Documents", "Pictures", None)
+folders.addNode("Documents", "Music", None)
+folders.addNode("Documents", "Code", None)
+folders.addNode("Documents Code", "PythonCode", None)
+print(folders.printTree("depth"))
+print(folders.printTree("breadth"))
